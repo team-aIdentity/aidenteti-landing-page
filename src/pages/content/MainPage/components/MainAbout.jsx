3d19d9421837a07a94ef71d0f9431b2a1b03ebf9
AnTaewoo <atw13730@gmail.com>
2024-07-19 19:06:24 +0900

    add: language setting Feat all DummyList: Change Language setting

--- src/pages/content/MainPage/components/MainAbout.jsx
+++ src/pages/content/MainPage/components/MainAbout.jsx
@@ -1,42 +1,43 @@
 import TitleDivider from "../../../common/TitleDivider/TitleDivider";
 import checkImg from "../../../../assets/about/check-img.png";
+import { useTranslation } from "react-i18next";
 
 export default function MainAbout() {
-  const descriptionList = [
-    "Reach new heights quickly with teamwork forged through competitions and hackathons.",
-    "Provide product services that meet the needs of your customers.",
-    "We use our youthful enthusiasm to grow with you.",
-    "With tons of networking experience, we build a variety of infrastructures.",
-  ];
-
-  const onContact = (e) => {
-    e.preventDefault();
-  };
+  const { t } = useTranslation();
 
   return (
     <>
       <TitleDivider title="About Aidenteti ?" number="01" />
       <p className="title">
-        Lorem ipsum dolor sit amet consectetur. Elementum nisl duis tortor sed.
-        Suspendisse lobortis vitae quis vehicula
-        <br />
-        pellentesque sit id. Urna posuere consequat velit vulputate faucibus
-        pretium arcu accumsan sit. Vel venenatis sapien.
+        <pre>{t(`MainAbout.title`)}</pre>
       </p>
       <div className="container">
         <div className="img-box"></div>
         <div className="description-box">
           <ul>
-            {descriptionList.map((value, index) => (
-              <li key={index}>
-                <img src={checkImg} alt="check-img" />
-                <p className="description">{value}</p>
-              </li>
-            ))}
+            <li>
+              <img src={checkImg} alt="check-img" />
+              <p className="description">{t(`MainAbout.description.1`)}</p>
+            </li>
+            <li>
+              <img src={checkImg} alt="check-img" />
+              <p className="description">{t(`MainAbout.description.2`)}</p>
+            </li>
+            <li>
+              <img src={checkImg} alt="check-img" />
+              <p className="description">{t(`MainAbout.description.3`)}</p>
+            </li>
+            <li>
+              <img src={checkImg} alt="check-img" />
+              <p className="description">{t(`MainAbout.description.4`)}</p>
+            </li>
           </ul>
-          <button className="contact" onClick={(e) => onContact(e)}>
-            contact
-          </button>
+          <a
+            href="https://mail.google.com/mail/?view=cm&amp;fs=1&amp;to=dulgi8655@gmail.com"
+            target="_blank"
+          >
+            CONTACT
+          </a>
         </div>
       </div>
     </>
--- src/pages/content/MainPage/components/MainServices.jsx
+++ src/pages/content/MainPage/components/MainServices.jsx
@@ -1,52 +1,35 @@
 import TitleDivider from "../../../common/TitleDivider/TitleDivider";
 import img from "../../../../assets/main/dummy-img.png";
+import { useTranslation } from "react-i18next";
 
 export default function MainServices() {
-  const serviceConponentsList = [
-    {
-      title: "Aidenteti Crew",
-      subTitle:
-        "To popularize Web3 Operate SNS marketing platforms such as YouTube / Instagram",
-      img: img,
-    },
-    {
-      title: "Product Development Agencies",
-      subTitle:
-        "We are an agency for website, application, AI, blockchain, etc. development.",
-      img: img,
-    },
-    {
-      title: "Sell and rent photobooths",
-      subTitle:
-        "Customize the software with your own photobooth to sell and rent.",
-      img: img,
-    },
-  ];
+  const serviceConponentsList = [img, img, img];
   return (
     <>
       <TitleDivider title="Services" number="02" />
       <ul className="service-components">
-        {serviceConponentsList.map((value, index) => (
-          <ServiceConponents
-            title={value.title}
-            subTitle={value.subTitle}
-            img={img}
-            key={index}
-          />
+        {serviceConponentsList.map((img, index) => (
+          <ServiceConponents img={img} key={index} index={index} />
         ))}
       </ul>
     </>
   );
 }
 
-const ServiceConponents = ({ title, subTitle, img }) => {
+const ServiceConponents = ({ index, img }) => {
+  const { t } = useTranslation();
+
   return (
     <li>
       <img src={img} alt="img" />
       <div className="components">
         <div className="title-box">
-          <p className="title">{title}</p>
-          <p className="sub-title">{subTitle}</p>
+          <p className="title">
+            <pre>{t(`MainServices.${index + 1}.title`)}</pre>
+          </p>
+          <p className="sub-title">
+            <pre>{t(`MainServices.${index + 1}.description`)}</pre>
+          </p>
         </div>
         <p className="learn-more">Learn more</p>
       </div>
