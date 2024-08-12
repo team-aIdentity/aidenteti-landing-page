f3811013e88e621bc917c3869fd7fd44ba1d1ed5
AnTaewoo <atw13730@gmail.com>
2024-08-12 21:07:13 +0900

    FEAT: add useGetIage

--- src/hooks/searchImgByTag.js
+++
@@ -1,9 +0,0 @@
-export const searchImgByTag = async (keyword) => {
-  const accessKey = await import.meta.env.VITE_UNSPLASY_ACCESS_KEY;
-  const url = `https://api.unsplash.com/photos/random?query=${keyword}&client_id=${accessKey}`;
-
-  const data = await (await fetch(url)).json();
-  const imgUrl = data.urls.full;
-
-  return imgUrl;
-};
--- src/pages/content/MainPage/components/MainServices.jsx
+++ src/pages/content/MainPage/components/MainServices.jsx
@@ -1,37 +1,24 @@
 import TitleDivider from "../../../common/TitleDivider/TitleDivider";
-import img from "../../../../assets/main/dummy-img.png";
-import { searchImgByTag } from "../../../../hooks/searchImgByTag";
 import { useTranslation } from "react-i18next";
 import { useEffect, useState } from "react";
+import { useGetImage } from "../hooks/useGetImage";
 
 export default function MainServices() {
-  const [serviceComponentsList, setServiceComponentsList] = useState([
-    img,
-    img,
-    img,
-  ]);
-  const [loading, setLoading] = useState(false);
+  const [imageList, setImageList] = useState(["", "", ""]);
 
-  // useEffect(() => {
-  //   searchImgHandle();
-  // }, []);
+  useEffect(() => {
+    const getImageHandle = async () => {
+      setImageList(useGetImage());
+    };
 
-  // const searchImgHandle = async () => {
-  //   let newImgList = serviceComponentsList;
-  //   newImgList[0] = await searchImgByTag("sns");
-  //   newImgList[1] = await searchImgByTag("blockchain");
-  //   newImgList[2] = await searchImgByTag("photo");
-
-  //   setServiceComponentsList(newImgList);
-  //   setLoading(true);
-  // };
-
+    getImageHandle();
+  }, []);
   return (
     <>
       <TitleDivider title="Services" number="02" />
       {true && (
         <ul className="service-components">
-          {serviceComponentsList.map((img, index) => (
+          {imageList.map((img, index) => (
             <ServiceConponents img={img} key={index} index={index} />
           ))}
         </ul>
@@ -45,7 +32,9 @@ const ServiceConponents = ({ index, img }) => {
 
   return (
     <li>
-      <img src={img} alt="img" />
+      <div className="image">
+        <img src={img} alt="img" />
+      </div>
       <div className="components">
         <div className="title-box">
           <p className="title">{t(`MainServices.${index + 1}.title`)}</p>
